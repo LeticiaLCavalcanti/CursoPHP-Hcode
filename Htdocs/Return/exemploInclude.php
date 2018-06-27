3c31bb2eab1d9fc4df3f2dd528e3500716f572af
LeticiaLCavalcanti <wmli515@gmail.com>
2018-06-27 02:22:17 -0300

    Session, return, while

---
+++ Htdocs/Return/exemploInclude.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once "exemploReturn.php";
+require_once "exemploReturn.php";
+
+$resultado = somar(32.5, 25.2);
+echo $resultado;
+
+?>
---
+++ Htdocs/Return/exemploReturn2.php
@@ -0,0 +1,7 @@
+<?php
+
+	include "exemplo_return.php";
+	$resultado = somar (10, 25);
+	echo $resultado;
+	
+?>
